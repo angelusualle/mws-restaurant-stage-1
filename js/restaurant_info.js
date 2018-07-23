ae2260d4bcf6ccf294e63fb75eb1c8a85c99489e
Barranco <Angel.Barranco@Emerson.com>
2018-07-23 10:19:59 -0400

    added map api tokens

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYW5nZWx1c3VhbGxlIiwiYSI6ImNqank5MWU1NjE3eHAza21raGtmeGcyN2QifQ.7GiGG6TQDTX6Q-1tVXx8lA',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
